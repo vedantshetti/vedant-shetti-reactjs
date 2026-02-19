e780a3759d78eea71cbe527db2ada00fe92f42fc
vedantshetti <vedantshetti123456@gmail.com>
2026-02-19 14:01:27 +0530

    added the moduled css

---
+++ 05-css/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Header/Header.module.css"
+
+const Header = () => {
+  return ( 
+    <div className='header'>
+        <h3>Vedant shetti</h3>
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
